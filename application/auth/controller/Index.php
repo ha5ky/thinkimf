f68c4611d3fd38a6981d38d7167c741e3294a918
shchenjianhua <unnnnn@foxmail.com>
2018-10-19 17:19:45 +0800

    add reg

--- application/auth/controller/Index.php
+++ application/auth/controller/Index.php
@@ -209,6 +209,8 @@ class Index extends Base
                 $api .= "?api_code=imfpwdfghjkdhgadv&action=login&email="
                     . $email . "&password=" . $loginPassword . "&username=" . $loginUsername;
                 $loginInfo = ImfHttpRequest($api);
+                $bbsuid = json_decode($loginInfo, true)['result']['uid'];
+                BBSdsetcookie('auth', BBSauthcode("$loginPassword\t$bbsuid", 'ENCODE'), 2596600, 1, false);
 
                 $this->success('注册成功，正在前往', $redirectUrl);
             };
