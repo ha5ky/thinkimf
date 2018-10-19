843b21a46ad503b079322f4434bcbfd83b97413f
shchenjianhua <unnnnn@foxmail.com>
2018-10-19 17:51:06 +0800

    add reg

--- application/auth/controller/Index.php
+++ application/auth/controller/Index.php
@@ -95,7 +95,7 @@ class Index extends Base
                     . $email . "&password=" . $loginPassword . "&username=" . $loginUsername;
                 $loginInfo = ImfHttpRequest($api);
                 $bbsuid = json_decode($loginInfo, true)['result']['uid'];
-                BBSdsetcookie('auth', BBSauthcode("$loginPassword\t$bbsuid", 'ENCODE'), 2596600, "thinkimf_", false);
+                BBSdsetcookie('auth', BBSauthcode("$loginPassword\t$bbsuid", 'ENCODE'), 2596600, "thinkimf_0ce7_", false);
                 /* var_dump($loginInfo);
                  exit;*/
                 $this->success('登录成功，正在前往', $redirectUrl);
@@ -210,7 +210,7 @@ class Index extends Base
                     . $email . "&password=" . $loginPassword . "&username=" . $loginUsername;
                 $loginInfo = ImfHttpRequest($api);
                 $bbsuid = json_decode($loginInfo, true)['result']['uid'];
-                BBSdsetcookie('auth', BBSauthcode("$loginPassword\t$bbsuid", 'ENCODE'), 2596600, "thinkimf_", false);
+                BBSdsetcookie('auth', BBSauthcode("$loginPassword\t$bbsuid", 'ENCODE'), 2596600, "thinkimf_0ce7_", false);
 
                 $this->success('注册成功，正在前往', $redirectUrl);
             };
