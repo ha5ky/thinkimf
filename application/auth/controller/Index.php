f82a8b0fd969d692ffddeafd8267f5f1b079a198
shchenjianhua <unnnnn@foxmail.com>
2018-10-19 16:35:29 +0800

    add reg

--- application/auth/controller/Index.php
+++ application/auth/controller/Index.php
@@ -185,6 +185,31 @@ class Index extends Base
                 session('username', ($user['name'] ?? $user['id'] ?? $user['email'] ?? $user['phone'] ?? $user['nickname']));
                 cookie('userid', $user['id']);
                 cookie('username', $user['name']);
+
+                if(is_SSL()){
+                    $httpPre = 'https://';
+                }else{
+                    $httpPre = 'http://';
+                }
+                if(strstr($_SERVER['HTTP_HOST'],"127.0.0.1")){
+                    $api = $httpPre."127.0.0.1:9091/api/imf.php";
+                }else{
+                    $api = $httpPre."bbs.thinkimf.com/api/imf.php";
+                }
+                if($email){
+                    $loginEmail = $email;
+                    $loginUsername = substr(md5($loginEmail),0,6);
+                    $loginPassword = substr(md5($loginEmail),0,6);
+                }
+                if($phone){
+                    $loginEmail = $phone."@thinkimf.com";
+                    $loginUsername = $phone;
+                    $loginPassword = substr(md5($loginEmail),0,6);
+                }
+                $api .= "?api_code=imfpwdfghjkdhgadv&action=login&email="
+                    .$email."&password=".$loginPassword."&username=".$loginUsername;
+                $loginInfo = ImfHttpRequest($api);
+
                 $this->success('注册成功，正在前往', $redirectUrl);
             };
         } else {
